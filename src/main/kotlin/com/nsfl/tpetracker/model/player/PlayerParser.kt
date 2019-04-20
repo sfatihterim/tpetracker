257c393ff5a2aa712d53885201b489f0f654613a
sfatihterim <sfatihterim@yandex.com>
2019-04-20 13:35:12 -0700

    Parse page count to speed up player parsing

--- src/main/kotlin/com/nsfl/tpetracker/model/player/PlayerParser.kt
+++ src/main/kotlin/com/nsfl/tpetracker/model/player/PlayerParser.kt
@@ -9,40 +9,20 @@ import java.util.regex.Pattern
 class PlayerParser {
 
     fun parseAll() = ArrayList<Player>().apply {
-        addAll(parsePlayers(Team.BALTIMORE_HAWKS, 4))
-        addAll(parsePlayers(Team.COLORADO_YETI, 4))
-        addAll(parsePlayers(Team.PHILADELPHIA_LIBERTY, 4))
-        addAll(parsePlayers(Team.YELLOWKNIFE_WRAITHS, 4))
-        addAll(parsePlayers(Team.ARIZONA_OUTLAWS, 4))
-        addAll(parsePlayers(Team.NEW_ORLEANS_SECOND_LINE, 4))
-        addAll(parsePlayers(Team.ORANGE_COUNTY_OTTERS, 4))
-        addAll(parsePlayers(Team.SAN_JOSE_SABERCATS, 4))
-        addAll(parsePlayers(Team.PALM_BEACH_SOLAR_BEARS, 4))
-        addAll(parsePlayers(Team.KANSAS_CITY_COYOTES, 4))
-        addAll(parsePlayers(Team.PORTLAND_PYTHONS, 4))
-        addAll(parsePlayers(Team.NORFOLK_SEAWOLVES, 4))
-        addAll(parsePlayers(Team.SAN_ANTONIO_MARSHALS, 4))
-        addAll(parsePlayers(Team.TIJUANA_LUCHADORES, 4))
-        addAll(parsePlayers(Team.FREE_AGENTS, 5))
-        addAll(parsePlayers(Team.QB_PROSPECTS, 3))
-        addAll(parsePlayers(Team.RB_PROSPECTS, 3))
-        addAll(parsePlayers(Team.WR_PROSPECTS, 3))
-        addAll(parsePlayers(Team.TE_PROSPECTS, 3))
-        addAll(parsePlayers(Team.OL_PROSPECTS, 3))
-        addAll(parsePlayers(Team.DE_PROSPECTS, 3))
-        addAll(parsePlayers(Team.DT_PROSPECTS, 3))
-        addAll(parsePlayers(Team.LB_PROSPECTS, 3))
-        addAll(parsePlayers(Team.CB_PROSPECTS, 3))
-        addAll(parsePlayers(Team.S_PROSPECTS, 3))
-        addAll(parsePlayers(Team.KP_PROSPECTS, 3))
+        Team.values().forEach { addAll(parsePlayers(it)) }
     }
 
-    private fun parsePlayers(team: Team, pageCount: Int): ArrayList<Player> {
+    private fun parsePlayers(team: Team): ArrayList<Player> {
 
         val playerList = ArrayList<Player>()
         val documentList = ArrayList<Document>()
 
-        for (i in 0..(pageCount - 1)) {
+        val firstDocument = Jsoup.connect("http://nsfl.jcink.net/index.php?showforum=${team.id}").get()
+        documentList.add(firstDocument)
+
+        val pageCount = parsePageCount(firstDocument.body().toString())
+
+        for (i in 1..(pageCount - 1)) {
             documentList.add(
                     Jsoup.connect(
                             "http://nsfl.jcink.net/index.php?showforum=${team.id}&st=${i * 15}"
@@ -83,6 +63,18 @@ class PlayerParser {
         }
 
         return playerList
+    }
+
+    private fun parsePageCount(document: String): Int {
+        return try {
+            val startIndex = document.indexOf("Pages:</a>")
+            val endIndex = document.indexOf(")", startIndex)
+            document.substring(startIndex, endIndex)
+                    .replace(Pattern.compile("[^0-9.]").toRegex(), "")
+                    .toInt()
+        } catch (exception: Exception) {
+            1
+        }
     }
 
     private fun parsePlayerInfo(elementString: String): String {
