sfatihterim/tpetracker

commit b8c285d77a58735ff98148d6a86aced831766b26

Switch team stats page to kotlinx-html

src/main/kotlin/com/nsfl/tpetracker/html/TeamStatsView.kt
@@ -1,0 +1,59 @@
+package com.nsfl.tpetracker.html
+
+import com.beust.klaxon.json
+import com.nsfl.tpetracker.model.player.ActivePlayer
+import com.nsfl.tpetracker.model.team.Team
+import kotlinx.html.*
+
+fun DIV.teamStatsView(nsflList: List<Pair<Team, List<ActivePlayer>>>, dsflList: List<Pair<Team, List<ActivePlayer>>>) {
+
+    div {
+        // this should really be classed and put in src/resources/main.css
+        style = "padding-left:80px;padding-right:80px;padding-top:40px;padding-bottom:20px;color:rgba(0,0,0,.87);font-size:28px;text-align:center"
+        +"Team Stats"
+    }
+
+    div {
+        // this should really be classed and put in src/resources/main.css
+        style = "padding-left:80px;padding-right:80px;padding-top:20px;padding-bottom:40px"
+        table(classes = "celled table ui") {
+            id = "table"
+            attributes["width"] = "100%%"
+            caption {}
+        }
+    }
+
+    script {
+        unsafe {
+            +"var dataSet = "
+            +json {
+                array(nsflList.map { pair ->
+                    array(
+                            "NSFL",
+                            "<a href=\"${pair.first.url}\">${pair.first.full}</a>",
+                            "${pair.second.getTotalTPE()}",
+                            "${pair.second.getAverageTPE()}",
+                            "${pair.second.getTotalTPE()}",
+                            "${pair.second.getAverageTPE()}",
+                            "${pair.second.getOffensiveTPE(false)}",
+                            "${pair.second.getDefensiveTPE(false)}"
+                    )
+                } + dsflList.map { pair ->
+                    array(
+                            "DSFL",
+                            "<a href=\"${pair.first.url}\">${pair.first.full}</a>",
+                            "${pair.second.getTotalTPE()}",
+                            "${pair.second.getAverageTPE()}",
+                            "${pair.second.getTotalTPE()}",
+                            "${pair.second.getAverageTPE()}",
+                            "${pair.second.getOffensiveTPE(true)}",
+                            "${pair.second.getDefensiveTPE(true)}"
+                    )
+                })
+            }.toJsonString()
+        }
+    }
+
+    script(src = "/static/js/team-stats-table-generator.js") {}
+
+}

src/main/kotlin/com/nsfl/tpetracker/html/AllPlayersView.kt
@@ -22,10 +22,6 @@ fun DIV.allPlayersView(allPlayers: List<ActivePlayer>) {
         }
     }
 
-    script(src = "/static/js/all-players-table-generator.js") {
-        defer = true
-    }
-
     script {
         unsafe {
             +"var dataSet = "
@@ -59,4 +55,6 @@ fun DIV.allPlayersView(allPlayers: List<ActivePlayer>) {
             }.toJsonString()
         }
     }
+
+    script(src = "/static/js/all-players-table-generator.js") {}
 }

src/main/kotlin/com/nsfl/tpetracker/html/HTMLGenerator.kt
@@ -56,16 +56,15 @@ class HTMLGenerator {
         }
     }
 
-    fun createTeamStatsPage(
-            nsflList: List<Pair<Team, List<ActivePlayer>>>,
-            dsflList: List<Pair<Team, List<ActivePlayer>>>
-    ) = TEAM_STATS_HTML.format(
-            nsflList.joinToString(",") { pair ->
-                "['NSFL','<a href=\"${pair.first.url}\">${pair.first.full}</a>','${pair.second.getTotalTPE()}','${pair.second.getAverageTPE()}','${pair.second.getTotalTPE()}','${pair.second.getAverageTPE()}','${pair.second.getOffensiveTPE(false)}','${pair.second.getDefensiveTPE(false)}']"
-            } + "," + dsflList.joinToString(",") { pair ->
-                "['DSFL','<a href=\"${pair.first.url}\">${pair.first.full}</a>','${pair.second.getTotalTPE()}','${pair.second.getAverageTPE()}','${pair.second.getTotalEffectiveTPE()}','${pair.second.getAverageEffectiveTPE()}','${pair.second.getOffensiveTPE(true)}','${pair.second.getDefensiveTPE(true)}']"
+    fun createTeamStatsPage(nsflList: List<Pair<Team, List<ActivePlayer>>>, dsflList: List<Pair<Team, List<ActivePlayer>>>) = buildString {
+        appendHTML().apply {
+            html {
+                navbarTemplate("Team Stats") {
+                    teamStatsView(nsflList, dsflList)
+                }
             }
-    )
+        }
+    }
 
     fun createPositionStatsPage(
             positionList: List<Pair<Position, List<ActivePlayer>>>
@@ -207,62 +206,6 @@ class HTMLGenerator {
 
     fun createErrorPage(error: String, copyPasta: String) = ERROR_HTML.format(error, copyPasta)
 
-    private fun List<Player>.getTotalTPE() = sumBy { it.currentTPE }
-
-    private fun List<Player>.getAverageTPE() = getTotalTPE() / countOrOne()
-
-    private fun List<Player>.getTotalEffectiveTPE() = sumBy { if (it.currentTPE > 250) 250 else it.currentTPE }
-
-    private fun List<Player>.getAverageEffectiveTPE() = getTotalEffectiveTPE() / countOrOne()
-
-    private fun List<Player>.getOffensiveTPE(dsfl: Boolean): Int {
-
-        val qbList = filter { it.position == Position.QB }
-                .sortedByDescending { it.currentTPE }
-
-        val offenseList = filter {
-            it.position == Position.RB
-                    || it.position == Position.WR
-                    || it.position == Position.TE
-        }.sortedByDescending { it.currentTPE }
-
-        return getPlayerTPESafe(qbList, 0, dsfl) +
-                getPlayerTPESafe(offenseList, 0, dsfl) +
-                getPlayerTPESafe(offenseList, 1, dsfl) +
-                getPlayerTPESafe(offenseList, 2, dsfl) +
-                getPlayerTPESafe(offenseList, 3, dsfl) +
-                getPlayerTPESafe(offenseList, 4, dsfl)
-    }
-
-    private fun List<Player>.getDefensiveTPE(dsfl: Boolean): Int {
-
-        val defenseList = filter {
-            it.position == Position.DE
-                    || it.position == Position.DT
-                    || it.position == Position.LB
-                    || it.position == Position.CB
-                    || it.position == Position.S
-        }.sortedByDescending { it.currentTPE }
-
-        return getPlayerTPESafe(defenseList, 0, dsfl) +
-                getPlayerTPESafe(defenseList, 1, dsfl) +
-                getPlayerTPESafe(defenseList, 2, dsfl) +
-                getPlayerTPESafe(defenseList, 3, dsfl) +
-                getPlayerTPESafe(defenseList, 4, dsfl) +
-                getPlayerTPESafe(defenseList, 5, dsfl) +
-                getPlayerTPESafe(defenseList, 6, dsfl) +
-                getPlayerTPESafe(defenseList, 7, dsfl) +
-                getPlayerTPESafe(defenseList, 8, dsfl) +
-                getPlayerTPESafe(defenseList, 9, dsfl) +
-                getPlayerTPESafe(defenseList, 10, dsfl)
-    }
-
-    private fun getPlayerTPESafe(playerList: List<Player>, index: Int, dsfl: Boolean) = try {
-        if (dsfl && playerList[index].currentTPE > 250) 250 else playerList[index].currentTPE
-    } catch (exception: Exception) {
-        50
-    }
-
     private fun <T, R : Comparable<R>> Iterable<T>.minBy(default: R, selector: (T) -> R): R {
         val min = minBy { selector.invoke(it) }
         return if (min == null) {
@@ -281,5 +224,4 @@ class HTMLGenerator {
         }
     }
 
-    private fun <T> Iterable<T>.countOrOne() = count().let { if (it == 0) 1 else it }
 }

src/main/kotlin/com/nsfl/tpetracker/html/TPECalcUtil.kt
@@ -1,0 +1,62 @@
+package com.nsfl.tpetracker.html
+
+import com.nsfl.tpetracker.model.player.Player
+import com.nsfl.tpetracker.model.position.Position
+
+fun List<Player>.getTotalTPE() = sumBy { it.currentTPE }
+
+fun List<Player>.getAverageTPE() = getTotalTPE() / countOrOne()
+
+fun List<Player>.getTotalEffectiveTPE() = sumBy { if (it.currentTPE > 250) 250 else it.currentTPE }
+
+fun List<Player>.getAverageEffectiveTPE() = getTotalEffectiveTPE() / countOrOne()
+
+fun List<Player>.getOffensiveTPE(dsfl: Boolean): Int {
+
+    val qbList = filter { it.position == Position.QB }
+            .sortedByDescending { it.currentTPE }
+
+    val offenseList = filter {
+        it.position == Position.RB
+                || it.position == Position.WR
+                || it.position == Position.TE
+    }.sortedByDescending { it.currentTPE }
+
+    return getPlayerTPESafe(qbList, 0, dsfl) +
+            getPlayerTPESafe(offenseList, 0, dsfl) +
+            getPlayerTPESafe(offenseList, 1, dsfl) +
+            getPlayerTPESafe(offenseList, 2, dsfl) +
+            getPlayerTPESafe(offenseList, 3, dsfl) +
+            getPlayerTPESafe(offenseList, 4, dsfl)
+}
+
+fun List<Player>.getDefensiveTPE(dsfl: Boolean): Int {
+
+    val defenseList = filter {
+        it.position == Position.DE
+                || it.position == Position.DT
+                || it.position == Position.LB
+                || it.position == Position.CB
+                || it.position == Position.S
+    }.sortedByDescending { it.currentTPE }
+
+    return getPlayerTPESafe(defenseList, 0, dsfl) +
+            getPlayerTPESafe(defenseList, 1, dsfl) +
+            getPlayerTPESafe(defenseList, 2, dsfl) +
+            getPlayerTPESafe(defenseList, 3, dsfl) +
+            getPlayerTPESafe(defenseList, 4, dsfl) +
+            getPlayerTPESafe(defenseList, 5, dsfl) +
+            getPlayerTPESafe(defenseList, 6, dsfl) +
+            getPlayerTPESafe(defenseList, 7, dsfl) +
+            getPlayerTPESafe(defenseList, 8, dsfl) +
+            getPlayerTPESafe(defenseList, 9, dsfl) +
+            getPlayerTPESafe(defenseList, 10, dsfl)
+}
+
+private fun getPlayerTPESafe(playerList: List<Player>, index: Int, dsfl: Boolean) = try {
+    if (dsfl && playerList[index].currentTPE > 250) 250 else playerList[index].currentTPE
+} catch (exception: Exception) {
+    50
+}
+
+fun <T> Iterable<T>.countOrOne() = count().let { if (it == 0) 1 else it }

src/main/resources/public/static/js/team-stats-table-generator.js
@@ -1,0 +1,15 @@
+$(document).ready(function () {
+    var t = $("#table").DataTable({
+        dom: "<'row'<'col-sm-6'B><'col-sm-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>",
+        scrollX: !0,
+        paging: !1,
+        order: [[0, "desc"]],
+        buttons: [{
+            text: "Toggle All", action: function () {
+                t.columns().visible(!t.column(0).visible())
+            }
+        }, {extend: 'colvis', text: 'Toggle Columns'}],
+        data: dataSet,
+        columns: [{title: "League"}, {title: "Name"}, {title: "Total TPE"}, {title: "Average TPE"}, {title: "Total Effective TPE"}, {title: "Average Effective TPE"}, {title: "Offensive TPE"}, {title: "Defensive TPE"}]
+    })
+});

commit 9b48ecfad4a84660e80251977ac91577732d6506

Fix Effective TPE bug
Made sure that effective TPE is used for DSFL players on team view.

Fixed issue introduced in b8c285d77a58735ff98148d6a86aced831766b26

src/main/kotlin/com/nsfl/tpetracker/html/TeamStatsView.kt
@@ -44,8 +44,8 @@ fun DIV.teamStatsView(nsflList: List<Pair<Team, List<ActivePlayer>>>, dsflList: 
                             "<a href=\"${pair.first.url}\">${pair.first.full}</a>",
                             "${pair.second.getTotalTPE()}",
                             "${pair.second.getAverageTPE()}",
-                            "${pair.second.getTotalTPE()}",
-                            "${pair.second.getAverageTPE()}",
+                            "${pair.second.getTotalEffectiveTPE()}",
+                            "${pair.second.getAverageEffectiveTPE()}",
                             "${pair.second.getOffensiveTPE(true)}",
                             "${pair.second.getDefensiveTPE(true)}"
                     )